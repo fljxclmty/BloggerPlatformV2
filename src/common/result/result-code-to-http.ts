9d4dcbfabd5fb8c1b6efe247108c78f0b6113fc1
Anton Ilchenko <anton56.234@gmail.com>
2026-04-12 21:08:14 +0500

    changed mapper

--- src/common/result/result-code-to-http.ts
+++ src/common/result/result-code-to-http.ts
@@ -1,18 +1,20 @@
-import {ResultStatus} from "./result-code";
-import {HttpStatus} from "../statuses";
+import { ResultStatus } from "./result-code";
+import { HttpStatus } from "../statuses";
 
 export const resultCodeToHttpException = (resultCode: ResultStatus): number => {
   switch (resultCode) {
     case ResultStatus.Success:
-      return HttpStatus.NoContent
+      return HttpStatus.NoContent; // 204
     case ResultStatus.BadRequest:
-      return HttpStatus.BadRequest;
+      return HttpStatus.BadRequest; // 400
     case ResultStatus.Forbidden:
-      return HttpStatus.Forbidden;
+      return HttpStatus.Forbidden; // 403
     case ResultStatus.Unauthorized:
-      return HttpStatus.Unauthorized;
+      return HttpStatus.Unauthorized; // 401
+    case ResultStatus.NotFound: //
+      return HttpStatus.NotFound; // 404
 
     default:
-      return HttpStatus.InternalServerError;
+      return HttpStatus.InternalServerError; // 500
   }
 };
